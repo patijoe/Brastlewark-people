5215fdb51490aac64367ad59d89c6c898aac2fba
Patricia Garcia <patijoe33@gmail.com>
2019-06-14 16:30:58 +0200

    map population

--- src/App.js
+++ src/App.js
@@ -19,19 +19,24 @@ class App extends React.Component {
   fetchPetition(){
     petition()
     .then(data => {
-      console.log('*', data);
+      console.log('*', data.Brastlewark[0].name);
+      this.setState({
+        population: data.Brastlewark
+      })
     })
+
   }
 
   render() {
     const {population} = this.state;
+    console.log('**', population);
 
     return (
       <div className="app">
       <ul className="population-list">
         {population.map(item => {
           return(
-            <li className="item-list">
+            <li className="item-list" key={item.id}>
               <h2 className="name">{item.name}</h2>
             </li>
           );
