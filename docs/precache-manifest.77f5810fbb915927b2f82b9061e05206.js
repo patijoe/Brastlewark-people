8137b2a4c4e4dfe972d51935af3dd7eccec32228
Patricia Garcia <patijoe33@gmail.com>
2019-06-17 10:41:06 +0200

    Add github pages

---
+++ docs/precache-manifest.77f5810fbb915927b2f82b9061e05206.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e488851c1e97c29b6a9fa85d663ca9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f84d8c2b1e7858700d27",
+    "url": "./static/css/main.e4917386.chunk.css"
+  },
+  {
+    "revision": "a8f05d41ff1e823d16b1",
+    "url": "./static/js/2.64e6472c.chunk.js"
+  },
+  {
+    "revision": "f84d8c2b1e7858700d27",
+    "url": "./static/js/main.2643ef7a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
